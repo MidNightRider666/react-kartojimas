8e862be3eee2ad8d80cc3df01496a384852bdb2b
MidNightRider666 <ledas850@gmail.com>
2022-05-04 14:01:45 +0300

    Styled App

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import React from "react";
 import { Button, Block, Hero } from "./Components";
 
 const App = () => {
-  return ( <div>
-    <>
-      <Hero type="success" children="Success Title" title="Success Subtitle"/>
-      <Block type="success">Success Block</Block>
-      <Button type="success" onclick="submit">Press me</Button>
-      </> 
+  return (
+    <div>
       <>
-      <Hero type="danger" children="Danger Title" title="Danger Subtitle"/>
-      <Block type="danger">Danger Block</Block>
-      <Button type="danger" onclick="reset">Press me</Button>
+        <Hero
+          type="success"
+          children="Success Title"
+          title="Success Subtitle"
+        />
+        <Block type="success">Success Block</Block>
+        <Button type="success" onclick="submit">
+          Press me
+        </Button>
       </>
       <>
-      <Hero type="primary" children="Primary Title" title="Primary Subtitle"/>
-      <Block type="primary">Primary Block</Block>
-      <Button type="primary" onclick="button">Press me</Button>
+        <Hero type="danger" children="Danger Title" title="Danger Subtitle" />
+        <Block type="danger">Danger Block</Block>
+        <Button type="danger" onclick="reset">
+          Press me
+        </Button>
       </>
-      </div>
+      <>
+        <Hero
+          type="primary"
+          children="Primary Title"
+          title="Primary Subtitle"
+        />
+        <Block type="primary">Primary Block</Block>
+        <Button type="primary" onclick="button">
+          Press me
+        </Button>
+      </>
+    </div>
   );
-}
+};
 
 export default App;
